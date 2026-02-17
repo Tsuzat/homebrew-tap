bee5ccacb2085dd126200411009fcfad3431ee82
Tsuzat <dev.tsuzat@gmail.com>
2026-02-17 12:39:03 +0530

    chore(nota): update to version 0.9.58-beta
    Update the cask to the latest beta release, including the new SHA256 checksums for both arm and intel architectures.

--- Casks/nota.rb
+++ Casks/nota.rb
@@ -1,9 +1,9 @@
 cask "nota" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.9.57-beta"
-  sha256 arm:   "30acd285edcf3632ca691f458319de725fe0ab2d024cfad4c0660537ba134d10",
-         intel: "b215e3e5928124f5a39aac9363b1bccff578000fad94535b26f804ef77258242"
+  version "0.9.58-beta"
+  sha256 arm:   "7219e918cf839135813d123514dfb061b46092746b05efd053cc505606cf3a71",
+         intel: "5516f10307d3c160fbc71269622c03ed85bb5a5af499a41aa0bcde98cca0df2e"
 
   url "https://github.com/Tsuzat/Nota/releases/download/release-v#{version}/Nota_#{version}_#{arch}.dmg"
   name "Nota"
