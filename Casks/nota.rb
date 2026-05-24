0af1e5222847770202fc69213c1aa98259d932c0
Tsuzat <dev.tsuzat@gmail.com>
2026-05-24 13:46:54 +0530

    chore: bump nota cask to version 0.9.68-beta

--- Casks/nota.rb
+++ Casks/nota.rb
@@ -1,9 +1,9 @@
 cask "nota" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.9.67-beta"
-  sha256 arm:   "2b8c1a03ad45fd4b34ad1e99f1488d9adfa38f10b195d27108149aa899c8ca1d",
-         intel: "39cb87003448d036ce73f9194ee020faea2dc8243c87ef1850531c5a40c10184"
+  version "0.9.68-beta"
+  sha256 arm:   "61bd0f07029db0d30e94389b853b16398792327add627ce0e14e57bf3e995209",
+         intel: "0b53a6531ffa19d11ddb975763efebf1bd712b95eab3679da3af3bf134020fdd"
 
   url "https://github.com/Tsuzat/Nota/releases/download/release-v#{version}/Nota_#{version}_#{arch}.dmg"
   name "Nota"
